24db746040dfa9a33668f0c0aef203a04faad8e0
patrick.reybaud <patrick.reybaud@electro-crea.com>
2022-02-28 11:39:38 +0100

    Sync for Colin

--- CM4/Full/objects.mk
+++ CM4/Full/objects.mk
@@ -3,7 +3,7 @@
 # Toolchain: GNU Tools for STM32 (9-2020-q2-update)
 ################################################################################
 
-USER_OBJS := /home/dev/STM32CubeIDE/workspace_1.8.0/SSS_MOD-747DISCO/CM4/Middlewares/Library/DSP\ Library/DSP\ Library/GCC/libarm_cortexM4lf_math.a
+USER_OBJS := /home/zhonx/Documents/Workspace_Cube_SSS/SSS_MOD-747DISCO/CM4/Middlewares/Library/DSP\ Library/DSP\ Library/GCC/libarm_cortexM4lf_math.a
 
 LIBS := -l:libtouchgfx-float-abi-hard.a
 
